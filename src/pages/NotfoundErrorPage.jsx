8578bc3dce77be998006da4f3612f4cf16e22c43
moDevsome <contact@mickael-outhier.fr>
2022-12-28 21:02:09 +0100

    Ajout de la propriété "message" à la page d'erreur.

--- src/pages/NotfoundErrorPage.jsx
+++ src/pages/NotfoundErrorPage.jsx
@@ -1,12 +1,18 @@
+import React from "react";
+
 /**
  * Page d'erreur 404, dans le cas où une erreur est provoquée par une page introuvable
  */
-function NotfoundErrorPage() {
+function NotfoundErrorPage(props) {
+
+    // Voilà à quoi serve les fragments
+    const message = typeof(props.message) === 'string' ? <React.Fragment><p>{ props.message }</p></React.Fragment> : '';
 
     return(
 
         <div className="page-wrapper" id="notfound-error-page">
             <b>Ceci est <i>un cul de sac</i> une erreur 404</b>
+            { message }
         </div>
     )
 
